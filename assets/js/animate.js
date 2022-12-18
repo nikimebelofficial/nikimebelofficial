a9808da1fb02578d8aee941c299c2f7a4477a379
nikimebelofficial <nikimebelofficial@gmail.com>
2022-12-18 14:52:58 +0700

    add effect index

---
+++ assets/js/animate.js
@@ -0,0 +1,41 @@
+const slider = document.querySelector(".services .swiper-wrapper");
+
+const caption = document.querySelector(".main-banner .caption");
+caption.classList.add("animate__animated", "animate__fadeInLeft");
+caption.style.setProperty("--animate-duration", "2s");
+
+const navBar = document.querySelector(".main-nav");
+navBar.classList.add("animate__animated", "animate__fadeInDown")
+navBar.style.setProperty("--animate-duration", "2s");
+
+const categories = document.querySelector(".who-we-are .categories");
+
+const textContent = document.querySelector(".who-we-are .text-content p");
+
+const whoAreWe = document.querySelector(".who-we-are .section-heading");
+
+
+
+// SCROLL DOWN
+window.addEventListener("scroll", function(){
+    let currentScrollPos = this.window.pageYOffset;
+    // if(currentScrollPos > 300){
+    //     slider.classList.add("animate__animated", "animate__fadeInUp");
+    //     slider.style.setProperty("--animate-duration", "1s");
+    // }
+
+    if(currentScrollPos > 1100){
+        categories.classList.add("animate__animated", "animate__fadeInLeft")
+        categories.style.setProperty("--animate-duration", "2s");
+        categories.style.opacity = "1";
+
+        textContent.classList.add("animate__animated", "animate__fadeInRight")
+        textContent.style.setProperty("--animate-duration", "2s");
+        textContent.style.opacity = "1";
+
+        whoAreWe.classList.add("animate__animated", "animate__fadeInDownBig")
+        whoAreWe.style.setProperty("--animate-duration", "1.5s");
+        whoAreWe.style.opacity = "1";
+    }
+
+});
